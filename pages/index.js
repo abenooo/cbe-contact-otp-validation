d57c369a5ea1e38c2d25cae8ff99ce02e5cd8cab
aR <abenezerkifle000@gmail.com>
2021-12-16 14:54:05 +0300

    make link from home page to validation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import React from 'react';
 export default function index() {
     return (
         <div>
-            <h1>Welcome to Abdu's page</h1>
-            <a href="/validation"></a>
+            {/* <h1>Welcome to Abdu's page</h1> */}
+      <a href='/validation'>validation</a>
         </div>
     )
 }
